f465761bef420d783986f8b5684a54f4b8531698
matty45 <matthewtoo5@googlemail.com>
2021-02-26 00:28:27 +0000

    failed implementation of the eldorito console

--- MattysHaloTool-DLL/main.cpp
+++ MattysHaloTool-DLL/main.cpp
@@ -6,12 +6,73 @@
 
 #pragma warning(disable:4996)
 
+Main::Main
+{
+	Console::Console Terminal;
+	std::map<std::string, std::shared_ptr<MainModule>> Commands;
 
+	HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleOutputCP(437);
+	unsigned int ConsoleWidth = 80;
+	CONSOLE_SCREEN_BUFFER_INFO ConsoleBuf;
+	if (GetConsoleScreenBufferInfo(hStdout, &ConsoleBuf))
+	{
+		ConsoleWidth = ConsoleBuf.dwSize.X;
+	}
+
+	SetConsoleTextAttribute(hStdout, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+	SetConsoleTextAttribute(hStdout, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+
+#ifdef _ELDEBUG
+	std::string buildType = "Debug ";
+#else
+	std::string buildType = "";
+#endif
+	std::cout << "ElDorito" << "\xC3\xC4\xC2\xC4\xC4\xC4\xC4\xC4\xC4\xB4 " << buildType << "Build Date: " << __DATE__ << " @ " << __TIME__ << std::endl;
+	Terminal.SetTextColor(Console::Input);
+
+	std::srand(GetTickCount());
+
+
+	Terminal.SetTextColor(Console::Green | Console::Bright);
+	std::cout << "Enter \"";
+	Terminal.SetTextColor(Console::Input);
+	std::cout << "help";
+	Terminal.SetTextColor(Console::Green | Console::Bright);
+	std::cout << "\" or \"";
+	Terminal.SetTextColor(Console::Input);
+	std::cout << "help (command)";
+	Terminal.SetTextColor(Console::Green | Console::Bright);
+
+	std::cout << "\" to get started!" << std::endl;
+
+	std::cout << "Please take the time to read our ";
+	Terminal.SetTextColor(Console::Input);
+	std::cout << "disclaimer";
+	Terminal.SetTextColor(Console::Green | Console::Bright);
+	std::cout << " as well." << std::endl;
+
+	// Register Modules
+	//PushModule<Test>("test");
+	//PushModule<Ammo>("ammo");
+	//PushModule<Camera>("camera");
+	//PushModule<DebugLogging>("debug");
+	//PushModule<Fov>("fov");
+	//PushModule<Globals>("global");
+	//PushModule<Godmode>("god");
+	//PushModule<Hud>("hud");
+	//PushModule<LoadLevel>("load");
+	//PushModule<Spawn>("spawn");
+	//PushModule<Information>("info");
+	//PushModule<ShowGameUI>("show_ui");
+	//PushModule<Disclaimer>("disclaimer");
+	//PushModule<ThirdPerson>("third");
+	Terminal.PrintLine();
+}
 
 DWORD WINAPI dwConsole(LPVOID)
 
 {
-	Console::AllocateConsole("Matthew's Halo Tool");
     return 0;
 }
 
--- MattysHaloTool-DLL/main.h
+++ MattysHaloTool-DLL/main.h
@@ -2,6 +2,42 @@
 #include <map>
 
 #include "console.h"
+#include "Modules/MainModule.h"
+#include "Utils/singleton.h"
 
-Console::Console Terminal;
-std::map<std::string, std::shared_ptr<ElModule>> Commands;
+class Main : public Utils::Singleton< Main >
+{
+public:
+
+	std::string GetDirectory();
+
+	void Tick(const std::chrono::duration<double>& DeltaTile);
+
+	void SetSessionMessage(const std::string& Message);
+
+	static void SetMainThreadID(size_t ThreadID)
+	{
+		MainThreadID = ThreadID;
+	}
+	static size_t GetMainThreadID()
+	{
+		return MainThreadID;
+	}
+
+	template<class T,
+		class = typename std::enable_if<std::is_base_of<MainModule, T>::value>::type>
+		void PushModule(const std::string& Name)
+	{
+		std::shared_ptr<T> Module = std::make_shared<T>();
+		Terminal.PushCommand(Name, Module);
+		Commands[Name] = Module;
+	}
+
+private:
+	// Thread
+	static size_t MainThreadID;
+
+	// Console
+	Console::Console Terminal;
+	std::map<std::string, std::shared_ptr<MainModule>> Commands;
+};
