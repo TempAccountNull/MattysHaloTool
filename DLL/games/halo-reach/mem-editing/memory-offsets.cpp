bdfc10c02b02d8d61ef125800332a4ddf98d62cd
Matthew <matthewtoo5@googlemail.com>
2021-08-19 17:39:48 +0100

    update memory offsets for 1.2448

--- DLL/games/halo-reach/mem-editing/memory-offsets.cpp
+++ DLL/games/halo-reach/mem-editing/memory-offsets.cpp
@@ -1,14 +1,14 @@
 #include "memory-offsets.h"
 //Put the addresses of game memory here!
 
-// 1.2241 offsets
+// 1.2448 offsets
 
-int haloreach::memory_offsets::pancam_offset = 0xA6D58;
+int haloreach::memory_offsets::pancam_offset = 0xA6EAC;
 
-int haloreach::memory_offsets::infinite_grenades_offset = 0x502594;
+int haloreach::memory_offsets::infinite_grenades_offset = 0x504B88;
 
-int haloreach::memory_offsets::hs_print_opcode_offset = 0x8DEE30;
+int haloreach::memory_offsets::hs_print_opcode_offset = 0x8E1F70;
 
-int haloreach::memory_offsets::ai_spawning_effects_offset = 0x5909DC;
+int haloreach::memory_offsets::ai_spawning_effects_offset = 0x59507C;
 
-int haloreach::memory_offsets::campaign_trick_offset = 0x4BFB7;
+int haloreach::memory_offsets::campaign_trick_offset = 0x5FE83;
